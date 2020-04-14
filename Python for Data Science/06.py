be7f88019493f384927d283fd3f4bc6c1b8ba18f
Nima Afshar <nafs080@aucklanduni.ac.nz>
2020-04-14 16:59:01 +1200

    Conditions and Branching

---
+++ Python for Data Science/06.py
@@ -0,0 +1,32 @@
+#Conditions and Branching
+
+i = 6
+print(i == 5)
+print(i > 5)
+
+print("a" == "b")
+
+age = 18
+
+if(age> 18):
+    print("you can enter")
+elif(age == 18):
+    print("go see Pink Floyd")
+
+else:
+    print("go see Meat Loaf")
+print("move on")
+#logic operator
+album_year = 1990
+
+if(album_year < 1980) or (album_year > 1989):
+    print("The album was made in the 70's or 90's")
+else:
+    print("The album was made in the 1980's")
+
+album__year = 1980
+
+if(album__year > 1979) and (album__year < 1990):
+    print("The album was made in the 80's")
+else:
+    print("The album was made in another decade")
