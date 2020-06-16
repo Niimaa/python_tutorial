b88150e17b52ad1a16106c41b03e64bb964f6731
Nima Afshar <nafs080@aucklanduni.ac.nz>
2020-06-16 13:03:10 +1200

    updated

---
+++ Python for Data Science/15.py
@@ -0,0 +1,12 @@
+#Application program interface
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+links={'GDP':'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/projects/coursera_project/clean_gdp.csv',
+       'unemployment':'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/projects/coursera_project/clean_unemployment.csv'}
+
+
+df = pd.read_csv(links['GPD'])
+
+# print(df.head())
