723f1269b7b2b991bfc46e009414a97357d0d17d
Nima Afshar <nafs080@aucklanduni.ac.nz>
2020-06-16 13:27:58 +1200

    Numbers&Operators

---
+++ Computational Python/Numbers&Operators.py
@@ -0,0 +1,57 @@
+my_int = 4
+my_float = 2.34
+print(my_float)
+print(type(my_int))
+result = my_int + my_float
+print(result)
+result = my_int - my_float
+print(result)
+result = my_int * my_float
+print(result)
+result = my_int / my_float
+print(result)
+result = my_int // my_float
+print(result)
+#exponents and powers
+result = 3 ** 2
+print(result)
+#modulus operator
+print(3%2)
+print(10%2)
+#order of operation
+print(3*2+4+(3-2)-1/2)
+#shortcuts
+my_int = 4
+my_int = my_int + 1
+print(my_int)
+#or
+my_int = 4
+my_int += 1
+print(my_int)
+
+my_int = 4
+my_int = my_int * 1
+print(my_int)
+#or
+my_int = 4
+my_int *= 1
+print(my_int)
+
+#Absolute and round values
+print(abs(-12))
+print(round(3.24))
+print(round(3.7))
+print(round(3.7365, 2))
+#comparison operators
+print(2==3)
+print(3<4)
+print(2!=3)
+print(2>=3)
+#type casting
+my_string_num = '100'
+my_string_num2 = '200'
+sum = my_string_num + my_string_num2
+print(sum)
+
+sum = int(my_string_num) + int(my_string_num2)
+print(sum)
