244049ecf966269dc405f7a53900c41c514638b8
Nima Afshar <nafs080@aucklanduni.ac.nz>
2020-06-19 15:50:34 +1200

    Tuples

---
+++ Computational Python/Tuples.py
@@ -0,0 +1,18 @@
+#define two lists
+month_1 = ['jan', 'feb', 'mar', 'apr']
+month_2 = month_1
+
+#change the item in list 1
+month_1[1] = 'February'
+print(month_1)
+print(month_2)
+
+#define two tuples
+tup_month_1 = ('jan', 'feb', 'mar', 'apr')
+tup_month_2 = tup_month_1
+
+#change the item in tuple 1 is not possible
+# tup_month_1[1] = 'February'
+print(tup_month_1)
+print(tup_month_2)
+
