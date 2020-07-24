b0e8e714558da0fdc1b944860c9287325b36e218
nima <nafs080@aucklanduni.ac.nz>
2020-07-24 13:39:39 +1200

    replace

--- Computational Python/replace_values.py
+++ Computational Python/replace_values.py
@@ -6,16 +6,13 @@ popularity = {'character':['spiderman','spiderman','superman','superman','batman
 
 dataframe1 = pd.DataFrame(popularity, index=pd.Index(['A','B','C','D','E','F','G','H','I','J'], name= 'Alphabet Index'), columns=pd.Index(['character','popularity','score1','score2','gender'], name= 'Attributes'))
 print(dataframe1)
-#reindex(rename the rows)
-dataframe2 = dataframe1.reindex(['E','B','C','D','A','F','G','I','H','J'])
-print(dataframe2)
 
-dataframe3 = dataframe1.reindex(columns= ['score1','popularity','character','score2','gender'])
-print(dataframe3)
+dataframe1['character'].replace('superman','superwoman', inplace= True)
+print(dataframe1)
 
-#rename the rows and columns
-dataframe3.rename(columns={'character':'super hero'}, inplace=True)
-dataframe3.rename(index={'A':'a','B':'b'}, inplace=True)
-print(dataframe3)
+#rename the value with the specific location
+dataframe1.iat[2,4]='female'
+print(dataframe1)
 
-#without inplace we can define a new dataframe but when inplace is true means we do all the changes in the current dataframe
+dataframe1.at['D','gender']='female'
+print(dataframe1)
